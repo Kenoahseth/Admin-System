698ca94877f35aa430de342b3f73e6d9fb2a4afc
Jerwin <caranyagan.jerwin.betorio@gmail.com>
2024-10-23 20:45:43 +0800

    Added Messages and Calendar Pages. Modified some pages.

--- script.js
+++ script.js
@@ -4,3 +4,51 @@ function showSection(sectionId) {
 
   document.getElementById(sectionId).style.display = "block";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    events: "load-events.php", // Load events from the server (PHP)
+    editable: true, // Allows editing events
+    selectable: true, // Allows selecting dates for new events
+    select: function (info) {
+      var title = prompt("Enter Event Title:");
+      if (title) {
+        // Send event to the server to save in database
+        fetch("add-event.php", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: title,
+            start: info.startStr,
+            end: info.endStr,
+          }),
+        })
+          .then((response) => response.json())
+          .then((event) => {
+            calendar.addEvent(event); // Add the event to the calendar
+          });
+      }
+      calendar.unselect();
+    },
+    eventClick: function (info) {
+      if (confirm("Are you sure you want to delete this event?")) {
+        fetch("delete-event.php", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: info.event.id,
+          }),
+        }).then(() => {
+          info.event.remove(); // Remove event from the calendar
+        });
+      }
+    },
+  });
+  calendar.render();
+});
